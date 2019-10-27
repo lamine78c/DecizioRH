58dab47fd21c4f4223ed274f5c9c9f32140c2411
Saliou DIALLO <saliu.diallo@gmail.com>
2019-10-27 15:44:21 +0100

    [EVOL] MAJ des des data fixtures

--- src/DataFixtures/VacationFixtures.php
+++ src/DataFixtures/VacationFixtures.php
@@ -17,20 +17,22 @@ class VacationFixtures extends Fixture implements DependentFixtureInterface
     {
 
         for ($i=0; $i < 10; $i++) {
-            
-            $vacation = new Vacation();            
-            $vacation->setPeriod(new \DateTime('2019-05-31'))
-                    ->setStartAt(new \DateTime())
+
+            $vacation = new Vacation();
+            $vacation->setPeriod('2020')
+                    ->setStartAt(new \DateTime('2019-06-01'))
+                    ->setEndAt(new \DateTime('2020-05-31'))
+                    ->setExpiredAt(new \DateTime('2021-05-31'))
                     ->setWin(8)
                     ->setSpent(2)
                     ->setUser($this->getReference('user_' . $i))
                     ->setVacationType($this->getReference(VacationTypeFixtures::VACATION_TYPE_REFERENCE_CA));
-            
-            $manager->persist($vacation);            
+
+            $manager->persist($vacation);
              $manager->flush();
         }
     }
-    
+
     public function getDependencies()
     {
         return array(
